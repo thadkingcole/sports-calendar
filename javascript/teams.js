6fa1d07982341ac2605db704d8b3bfaff6379b47
Thaddeus Cole <thadcole@duck.com>
2022-08-07 13:35:24 -0400

    clicking 'NFL' adds checkmarks for each team

--- javascript/teams.js
+++ javascript/teams.js
@@ -1,5 +1,9 @@
+// ELEMENTS
+const teamListEl = document.getElementById("teamList");
+
 // GLOBAL VARIABLES
 let NFLteams;
+let teams;
 
 async function show(league) {
   console.log(league);
@@ -13,8 +17,7 @@ async function show(league) {
       break;
 
     case "NFL":
-      // NFLteams not previously saved
-      // prevents API from being called unnecessarily
+      // only call API if NFLteams not loaded previously
       if (!NFLteams) {
         console.log("football");
         const response = await fetch(
@@ -22,7 +25,7 @@ async function show(league) {
         );
         const teamsObj = await response.json();
         NFLteams = teamsObj.sports[0].leagues[0].teams;
-        // console.log(NFLteams);
+        teams = NFLteams;
       }
       break;
 
@@ -34,4 +37,42 @@ async function show(league) {
       console.log("no league selected");
       break;
   }
+
+  // NFL TEAMS WORK HERE
+  // create list of teams
+  teams.forEach((team) => {
+    // create checkbox with team logo and team name
+
+    // parent form-check
+    const formCheck = document.createElement("div");
+    formCheck.className = "form-check";
+
+    // checkbox
+    const checkbox = document.createElement("input");
+    checkbox.className = "form-check-input";
+    checkbox.type = "checkbox";
+    checkbox.value = "";
+    checkbox.id = team.team.abbreviation;
+    formCheck.appendChild(checkbox);
+
+    // label: (logo) team name
+    const teamLabel = document.createElement("label");
+    teamLabel.className = "form-check-label";
+    teamLabel.for = team.team.abbreviation;
+
+    // make team logo
+    const logoImg = document.createElement("img");
+    logoImg.src = team.team.logos[0].href;
+    logoImg.alt = team.team.abbreviation;
+    logoImg.width = "20";
+    logoImg.className = "me-1";
+
+    // add logo and team name text
+    teamLabel.appendChild(logoImg);
+    logoImg.insertAdjacentText("afterend", team.team.displayName);
+
+    // add label to form & team list
+    formCheck.appendChild(teamLabel);
+    teamListEl.appendChild(formCheck);
+  });
 }
