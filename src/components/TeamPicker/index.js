215ef01f4170e72522c421086e26159751de2e2c
Thaddeus Cole <thadcole@duck.com>
2022-08-20 09:57:26 -0400

    clicking a league prints teams on sidebar

--- src/components/TeamPicker/index.js
+++ src/components/TeamPicker/index.js
@@ -4,11 +4,28 @@ import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import Offcanvas from "react-bootstrap/Offcanvas";
 
-const TeamPicker = () => {
+function TeamPicker() {
   const [showCanvas, setShowCanvas] = useState(false);
+  const [teams, setTeams] = useState([]);
 
   const handleCloseCanvas = () => setShowCanvas(false);
   const handleShowCanvas = () => setShowCanvas(true);
+  const handleLeagueClick = (e) => showTeams(e.target.text.toLowerCase());
+
+  async function showTeams(league) {
+    const leagues = {
+      mlb: "baseball",
+      nba: "basketball",
+      nfl: "football",
+      nhl: "hockey",
+    };
+    const url = `http://site.api.espn.com/apis/site/v2/sports/${leagues[league]}/${league}/teams`;
+    console.log(url);
+    const res = await fetch(url);
+    const data = await res.json();
+    const { teams } = data.sports[0].leagues[0];
+    setTeams(teams);
+  }
 
   return (
     <div className="bg-primary">
@@ -21,19 +38,22 @@ const TeamPicker = () => {
         </Offcanvas.Header>
         <Offcanvas.Body>
           <DropdownButton id="selectLeagueDD" title="Select League">
-            <Dropdown.Item href="#/MLB">MLB</Dropdown.Item>
-            <Dropdown.Item href="#/NBA">NBA</Dropdown.Item>
-            {/* <Dropdown.Item href="#/NCAA Football">NCAA Football</Dropdown.Item> */}
-            {/* <Dropdown.Item href="#/NCAA Basketball">
-              NCAA Basketbal
-            </Dropdown.Item> */}
-            <Dropdown.Item href="#/NFL">NFL</Dropdown.Item>
-            <Dropdown.Item href="#/NHL">NHL</Dropdown.Item>
+            <Dropdown.Item onClick={handleLeagueClick}>MLB</Dropdown.Item>
+            <Dropdown.Item onClick={handleLeagueClick}>NBA</Dropdown.Item>
+            <Dropdown.Item disabled onClick={handleLeagueClick}>
+              NCAA Football
+            </Dropdown.Item>
+            <Dropdown.Item disabled onClick={handleLeagueClick}>
+              NCAA Basketball
+            </Dropdown.Item>
+            <Dropdown.Item onClick={handleLeagueClick}>NFL</Dropdown.Item>
+            <Dropdown.Item onClick={handleLeagueClick}>NHL</Dropdown.Item>
           </DropdownButton>
+          {teams.map((team) => team.team.displayName)}
         </Offcanvas.Body>
       </Offcanvas>
     </div>
   );
-};
+}
 
 export default TeamPicker;
