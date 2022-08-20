9c73d2f5ed4bd4411e7a3b57e01a0c4b98d0f592
Thaddeus Cole <thadcole@duck.com>
2022-08-20 11:41:32 -0400

    list of teams displays as checkboxes

--- src/components/TeamPicker/index.js
+++ src/components/TeamPicker/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
+import Form from "react-bootstrap/Form";
 import Offcanvas from "react-bootstrap/Offcanvas";
 
 function TeamPicker() {
@@ -20,10 +21,10 @@ function TeamPicker() {
       nhl: "hockey",
     };
     const url = `http://site.api.espn.com/apis/site/v2/sports/${leagues[league]}/${league}/teams`;
-    console.log(url);
     const res = await fetch(url);
     const data = await res.json();
     const { teams } = data.sports[0].leagues[0];
+    console.log(teams);
     setTeams(teams);
   }
 
@@ -32,12 +33,20 @@ function TeamPicker() {
       <Button variant="primary" onClick={handleShowCanvas}>
         Pick Teams
       </Button>
-      <Offcanvas show={showCanvas} onHide={handleCloseCanvas}>
+      <Offcanvas
+        className="text-bg-dark"
+        show={showCanvas}
+        onHide={handleCloseCanvas}
+      >
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Pick teams to add to the calendar</Offcanvas.Title>
+          <Offcanvas.Title>Pick teams to add to the calendar:</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <DropdownButton id="selectLeagueDD" title="Select League">
+          <DropdownButton
+            className="mb-3"
+            id="selectLeagueDD"
+            title="Select League"
+          >
             <Dropdown.Item onClick={handleLeagueClick}>MLB</Dropdown.Item>
             <Dropdown.Item onClick={handleLeagueClick}>NBA</Dropdown.Item>
             <Dropdown.Item disabled onClick={handleLeagueClick}>
@@ -49,7 +58,24 @@ function TeamPicker() {
             <Dropdown.Item onClick={handleLeagueClick}>NFL</Dropdown.Item>
             <Dropdown.Item onClick={handleLeagueClick}>NHL</Dropdown.Item>
           </DropdownButton>
-          {teams.map((team) => team.team.displayName)}
+          <Form>
+            {teams.map((team) => {
+              return (
+                <Form.Check key={team.team.uid}>
+                  <Form.Check.Input type="checkbox"></Form.Check.Input>
+                  <Form.Check.Label>
+                    <img
+                      src={team.team.logos[0].href}
+                      alt={team.team.displayName}
+                      width="25"
+                      className="me-2"
+                    />
+                    {team.team.displayName}
+                  </Form.Check.Label>
+                </Form.Check>
+              );
+            })}
+          </Form>
         </Offcanvas.Body>
       </Offcanvas>
     </div>
