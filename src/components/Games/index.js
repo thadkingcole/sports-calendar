96f19f4d3c8ca6556d5ef865b140e362f5633866
Thaddeus Cole <thadcole@duck.com>
2022-08-25 12:38:54 -0500

    Game comp has State object with all picked games

--- src/components/Games/index.js
+++ src/components/Games/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
-import LEAGUES from "../../constants";
 
 function Games({ myTeams }) {
   // STATE VARIABLES
@@ -8,21 +7,27 @@ function Games({ myTeams }) {
 
   // HOOKS
   useEffect(() => {
-    // each time myTeams changes, need to get new list of games.
-    // start with getting list of games for each team
-    for (let id in myTeams) {
+    // each time myTeams changes, get new list of games.
+    // check for games that don't have my teams
+    setMyGames((current) => {
+      return Object.keys(current).filter((gameId) => {
+        Object.keys(myTeams).includes(current[gameId].myTeamId);
+      });
+    });
+    // then get a list of games for each team
+    Object.keys(myTeams).forEach((id) => {
       // url to be colled for the team
       const url = `http://sports.core.api.espn.com/v2/sports/${
-        LEAGUES[myTeams[id]]
-      }/leagues/${myTeams[id]}/seasons/2022/teams/${id
+        myTeams[id].sport
+      }/leagues/${myTeams[id].league}/seasons/2022/teams/${id
         .split("t:")
         .pop()}/events?limit=200`;
-      getGameInfo(url);
-    }
+      getGameInfo(id, url);
+    });
   }, [myTeams]);
 
   // HELPER FUNCTIONS
-  async function getGameInfo(url) {
+  async function getGameInfo(teamId, url) {
     // list of events
     const eventsRes = await fetch(url);
     const events = await eventsRes.json();
@@ -39,19 +44,7 @@ function Games({ myTeams }) {
       // status
       const statusRes = await fetch(game.competitions[0].status.$ref);
       const status = await statusRes.json();
-      // print to console.log (for now)
-      // console.log({
-      //   [game.uid]: {
-      //     date: new Date(game.date),
-      //     teams: game.name,
-      //     status: status.type.shortDetail,
-      //     awayScore: lp && lp.awayScore,
-      //     homeScore: lp && lp.homeScore,
-      //   },
-      // });
-
-      // ! this is what i'll need later
-      // TODO currently no way to delete teams unchecked
+      // save to state
       setMyGames((prev) => ({
         ...prev,
         [game.uid]: {
@@ -60,37 +53,13 @@ function Games({ myTeams }) {
           status: status.type.shortDetail,
           awayScore: lp && lp.awayScore,
           homeScore: lp && lp.homeScore,
+          myTeamId: teamId,
         },
       }));
-
-      // return {
-      //   uid: game.uid,
-      //   date: new Date(game.date),
-      //   teams: game.name,
-      //   status: status.type.shortDetail,
-      //   awayScore: lp && lp.awayScore,
-      //   homeScore: lp && lp.homeScore,
-      // };
     });
   }
 
-  return (
-    <Container>
-      {/* for each selected team... */}
-      {/* {Object.keys(myTeams).map(async (id) => {
-        const url = `http://sports.core.api.espn.com/v2/sports/${
-          LEAGUES[myTeams[id]]
-        }/leagues/${myTeams[id]}/seasons/2022/teams/${id
-          .split("t:")
-          .pop()}/events?limit=200`;
-        // get each game they are in
-        const gameData = await getGameInfo(url);
-
-        // TODO actual return value
-        return <div key={id}>{id}</div>;
-      })} */}
-    </Container>
-  );
+  return <Container>coming soon...</Container>;
 }
 
 export default Games;
