2cd6ae0c507065559223f0a6ba888ccab4638fab
Thaddeus Cole <thadcole@duck.com>
2022-08-25 22:26:43 -0500

    can print games to the page
    need to format games and fix  error in console

--- src/components/Games/index.js
+++ src/components/Games/index.js
@@ -9,11 +9,11 @@ function Games({ myTeams }) {
   useEffect(() => {
     // each time myTeams changes, get new list of games.
     // check for games that don't have my teams
-    setMyGames((current) => {
-      return Object.keys(current).filter((gameId) => {
-        Object.keys(myTeams).includes(current[gameId].myTeamId);
-      });
-    });
+    setMyGames((current) =>
+      Object.keys(current).filter((gameId) =>
+        Object.keys(myTeams).includes(current[gameId].myTeamId)
+      )
+    );
     // then get a list of games for each team
     Object.keys(myTeams).forEach((id) => {
       // url to be colled for the team
@@ -36,6 +36,8 @@ function Games({ myTeams }) {
       const gameRes = await fetch(event.$ref);
       const game = await gameRes.json();
       // game situation
+      // TODO getting error from baseball, no situation maybe?
+      // console.log(game.competitions[0])
       const sitRes = await fetch(game.competitions[0].situation.$ref);
       const sit = await sitRes.json();
       // last play (doesn't exist if game hasn't happened yet)
@@ -59,7 +61,24 @@ function Games({ myTeams }) {
     });
   }
 
-  return <Container>coming soon...</Container>;
+  return (
+    <Container>
+      {Object.entries(myGames)
+        .sort(([_, a], [__, b]) => a.date - b.date)
+        .map(([id, game]) => {
+          return (
+            <div
+              key={id}
+              className="text-white"
+              style={{ backgroundColor: "#" + myTeams[game.myTeamId].color }}
+            >
+              {game.date.toDateString()} {game.awayScore} {game.teams}{" "}
+              {game.homeScore} {game.status}
+            </div>
+          );
+        })}
+    </Container>
+  );
 }
 
 export default Games;
